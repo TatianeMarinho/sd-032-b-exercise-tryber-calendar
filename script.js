7d4d6543df5d322ee28b143253385689bd609624
TatianeMarinho <devtatianemarinho@gmail.com>
2023-04-13 10:00:40 -0400

    Não consigo resolver o problemaaaaaaaaaaaaaaa

--- script.js
+++ script.js
@@ -15,25 +15,30 @@ const createDaysOfTheWeek = () => {
 
 createDaysOfTheWeek();
 
+
 const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-// Exercício 1 – Crie um calendário dinamicamente:
-function days() {
-  const tagUlD = getElementById('days');
+// Escreva seu código abaixo.
+//1 - Crie um calendário dinamicamente
+const ul = document.querySelector('#days'); // capturei a ul.
 
-  for (let i = 0; i < decemberDaysList.length; i +=1) {
-    const itemDay = decemberDaysList[i];
-    const liDays = document.createElement('li');
-    liDays.innerHTML = itemDay;
-    liDays.className = 'day';
-    
-    if (itemDay === 24 || itemDay === 25 || itemDay === 31) {
-      liDays.classList.add = 'hodiday';
-    }else if (itemDay === 4 || itemDay === 11 ||itemDay === 18 || itemDay === 25) {
-      liDays.classList.add = 'friday';
+
+function numberDays() {
+  for (let i of decemberDaysList){
+    const li = document.createElement('li');
+    li.innerText = i;
+
+    if( i === 24 || i === 25 || i===31){
+      li.classList.add = 'hodiday';
+    }else if (i === 4 || i === 11 || i === 18 || i === 25){
+      li.classList.add = 'friday';
     };
+    li.classList.add = 'day';
+    ul.appendChild(li);
   };
-  tagUlD.appendChild('liDays');
+    
 };
+numberDays();
 
-days();
+
+ 
