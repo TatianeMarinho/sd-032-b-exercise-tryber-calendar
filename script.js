715a8ee3bd0e32587a1d81cb721c43d7ae942a18
TatianeMarinho <devtatianemarinho@gmail.com>
2023-04-12 20:46:06 -0400

    enviando o desafio 1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const { get } = require("cypress/types/lodash");
 
 const createDaysOfTheWeek = () => {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
@@ -16,5 +17,23 @@ createDaysOfTheWeek();
 
 const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-// aEscreva seu código abaixo.
+// Exercício 1 – Crie um calendário dinamicamente:
+function days() {
+  const tagUlD = getElementById('days');
 
+  for (let i = 0; i < decemberDaysList.length; i +=1) {
+    const itemDay = decemberDaysList[i];
+    const liDays = document.createElement('li');
+    liDays.innerHTML = itemDay;
+    liDays.className = 'day';
+    
+    if (itemDay === 24 || itemDay === 25 || itemDay === 31) {
+      liDays.classList.add = 'hodiday';
+    }else if (itemDay === 4 || itemDay === 11 ||itemDay === 18 || itemDay === 25) {
+      liDays.classList.add = 'friday';
+    };
+  };
+  tagUlD.appendChild('liDays');
+};
+
+days();
