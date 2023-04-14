95aaa1ccf6a1edbd14adbb0924e1d84a57d1c410
TatianeMarinho <devtatianemarinho@gmail.com>
2023-04-14 00:44:57 -0400

    desafio 3

--- script.js
+++ script.js
@@ -41,23 +41,41 @@ days();
 
 //Exercício 2 – Implemente uma função que muda a cor de fundo dos dias que possuem a classe "holiday"
 const colorHoliday = () => {
-const buttonHoliday = document.getElementById("btn-holiday");
-const listHolidays = document.getElementsByClassName('holiday');
-const colorStart = "rgb(238,238,238)";
-const colorOn = 'green';
+  const buttonHoliday = document.getElementById("btn-holiday");
+  const listHolidays = document.getElementsByClassName('holiday');
+  const colorStart = "rgb(238,238,238)";
+  const colorOn = 'green';
 
-buttonHoliday.addEventListener('click',() =>{
-    for (let i = 0; i < listHolidays.length; i += 1) {
-      const holiday = listHolidays[i];
-      const elementNewColor = holiday.style.backgroundColor === colorOn;
+  buttonHoliday.addEventListener('click',() =>{
+      for (let i = 0; i < listHolidays.length; i += 1) {
+        const holiday = listHolidays[i];
+        const elementNewColor = holiday.style.backgroundColor === colorOn;
 
-      if(elementNewColor) {
-        listHolidays[i].style.backgroundColor = colorStart; 
-      }else {
-        listHolidays[i].style.backgroundColor = colorOn;
+        if(elementNewColor) {
+          listHolidays[i].style.backgroundColor = colorStart; 
+        }else {
+          listHolidays[i].style.backgroundColor = colorOn;
+        }
+      }
+    });
+};
+colorHoliday();
+
+//Exercício 3 – Implemente uma função que modifica o texto exibido nos dias que são sextas-feiras
+const friday = () => {
+  const buttonFriday = document.getElementById("btn-friday");
+  const listFridays = document.getElementsByClassName('friday');
+  const newText = "Sextouuuu!!!!!!!";
+
+  buttonFriday.addEventListener('click',() =>{
+    for (let i = 0; i < listFridays.length; i += 1) {
+      if(listFridays[i] !== newText) {
+        listFridays[i].innerText = newText; 
+      }else if (listFridays[i] === newText){
+        listFridays[i].innerText = listFridays[i];
       }
     }
   });
 };
-colorHoliday();
+friday();
  
